679a1c06552e1fbad255d959071b33d8b5566036
lijinting01 <lijinting01@picc.com.cn>
2016-06-22 09:18:11 +0800

    1、去掉多余的部分。

--- pizza-users/src/test/java/cc/picc/web/RegisterControllerTest.java
+++ pizza-users/src/test/java/cc/picc/web/RegisterControllerTest.java
@@ -1,25 +1,37 @@
 package cc.picc.web;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
+import cc.picc.component.RegisterForm;
 import cc.picc.component.RegisterService;
+import cc.picc.entity.UserBasics;
 
 @RunWith(value = MockitoJUnitRunner.class)
-@ContextConfiguration(classes = { RegisterTestContext.class })
 public class RegisterControllerTest {
 
 	@Mock
 	private RegisterService registerService;
+
+	@Mock
+	private UserBasics sampleUserBasics;
+
+	@Before
+	public void setUp() {
+		when(registerService.registerNewUser(any(RegisterForm.class)))
+				.thenReturn(sampleUserBasics);
+	}
 
 	@Test
 	public void testRegisterForm() throws Exception {
